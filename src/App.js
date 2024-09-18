406b552239e9e4b57507ab9496fdc6c8f572668c
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2024-09-18 21:41:27 +0530

    Work on Input Box and its Inner values

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
    <p>Hey I Am Akash</p>
    <p>Not to Anything</p>
+   <input type='text' placeholder='Enter your Name' name='Doctor' id='name' className='local'/>
+   <br/>
+   <br/>
+   <br/>
    <img title='this is leaf' src='https://th.bing.com/th/id/OIP.MPP1fw6m0Gy1xNGZF2ioIAHaFG?rs=1&pid=ImgDetMain' alt='prompt.jpg'/>
     </div>
   );
