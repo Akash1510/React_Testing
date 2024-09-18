7895bda5aa364eb541342a0d0dbbe1f2e6fdf70c
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2024-09-18 21:42:27 +0530

    test Cases on the Input / textbox

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import {render,screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import App from './App'
 
@@ -6,24 +6,61 @@ import App from './App'
 // 1 > test case for the text checking on the screen
 
 
-test('Check the Text', () => { 
-  render(<App/>)
+test('Check the Text', () => {
+  render(<App />)
   const textElement = screen.getByText(/Hey I Am Akash/i)
   expect(textElement).toBeInTheDocument();
- })
+})
 
- test('Now for the test 2',
-  ()=>{
-    render(<App/>)
+test('Now for the test 2',
+  () => {
+    render(<App />)
     const text = screen.getByText(/Not to Anything/i)
     expect(text).toBeInTheDocument()
-    }
- )
+  }
+)
 
- test("to Check the Leaf Title" 
-  ,()=>{
-    render(<App/>)
+test("to Check the Leaf Title"
+  , () => {
+    render(<App />)
     const title = screen.getByTitle(/this is leaf/i)
     expect(title).toBeInTheDocument()
   }
- )
+)
+
+
+//  Now to test the Input Box id , placeholder , name , value ,type
+
+
+// to test the input box
+
+test("Testing of the InputBox",
+  () => {
+    render(<App />)
+    const CheckInputBox = screen.getByRole("textbox")
+    expect(CheckInputBox).toBeInTheDocument()
+
+  }
+)
+
+// to check or test the placeholder
+
+test("check that placeholder is present or not", () => {
+  render(<App />)
+
+  const textplaceholder = screen.getByPlaceholderText(/Enter your Name/i)
+  expect(textplaceholder).toBeInTheDocument()
+
+})
+
+// to check the name attribute
+
+test("Check the Attributes in the testbox",()=>{
+  render(<App />)
+
+  const nameplace = screen.getByRole(/textbox/)
+  expect(nameplace).toHaveAttribute("name","Doctor")
+  // check for the id is present or not
+  expect(nameplace).toHaveAttribute("id","name")
+  expect(nameplace).toHaveAttribute('class','local')
+})
