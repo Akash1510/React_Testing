f4793e7eecfad7c1014706f8504e05cc48f27823
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2024-09-20 12:57:03 +0530

    change in Spacing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   <p>Hey I Am Akash</p>
-   <p>Not to Anything</p>
-   <input type='text' placeholder='Enter your Name' name='Doctor' id='name' className='local'/>
-   <br/>
-   <br/>
-   <br/>
-   <img title='this is leaf' src='https://th.bing.com/th/id/OIP.MPP1fw6m0Gy1xNGZF2ioIAHaFG?rs=1&pid=ImgDetMain' alt='prompt.jpg'/>
+      <p>Hey I Am Akash</p>
+      <p>Not to Anything</p>
+      <input type='text' placeholder='Enter your Name' name='Doctor' id='name' className='local' />
+      <br />
+      <br />
+      <br />
+      <img title='this is leaf' src='https://th.bing.com/th/id/OIP.MPP1fw6m0Gy1xNGZF2ioIAHaFG?rs=1&pid=ImgDetMain' alt='prompt.jpg' />
 
     </div>
   );
