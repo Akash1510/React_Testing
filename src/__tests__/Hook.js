4cc7a430a6f0e965aa62b925d098b63df9523115
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2024-09-22 12:04:02 +0530

    Using the BeforeEach hook

--- src/__tests__/Hook.js
+++ src/__tests__/Hook.js
@@ -5,8 +5,13 @@ import App from "../App";
 
 //  BeforeAll hook ->this hook fires one time before execution of the test cases
 
-beforeAll(() => {
-    console.log("*********Function is Running that is Before all******")
+// beforeAll(() => {
+//     console.log("*********Function is Running that is Before all******")
+// })
+
+// Now use the BeforeEach Hook ->Excution Depends on the No of Testcases that time it excutes
+beforeEach(()=>{
+    console.log("___________Before Each Is Excuted __________")
 })
 
 test('Check this test case', () => {
@@ -14,4 +19,21 @@ test('Check this test case', () => {
     const text = screen.getByText(/Hey I Am Akash/i)
     expect(text).toBeInTheDocument()
 })
+test('Check this test case 2', () => {
+    render(<App />)
+    const text = screen.getByText(/Not to Anything/i)
+    expect(text).toBeInTheDocument()
+})
+test('Check this test case 3', () => {
+    render(<App />)
+    const text = screen.getByText(/This is Your Name/i)
+    expect(text).toBeInTheDocument()
+})
+test('Check this test case 4', () => {
+    render(<App />)
+    const text = screen.getByText(/Hey I Am Akash/i)
+    expect(text).toBeInTheDocument()
+})
+
+
 
