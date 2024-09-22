5910668d2617719b1a043ed36213b854cd1d9636
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2024-09-22 12:17:09 +0530

    using the afterEach hook

--- src/__tests__/Hook.js
+++ src/__tests__/Hook.js
@@ -16,21 +16,25 @@ import App from "../App";
 
 test('Check this test case', () => {
     render(<App />)
+    console.log("testcase 1")
     const text = screen.getByText(/Hey I Am Akash/i)
     expect(text).toBeInTheDocument()
 })
 test('Check this test case 2', () => {
     render(<App />)
+    console.log("testcase 2")
     const text = screen.getByText(/Not to Anything/i)
     expect(text).toBeInTheDocument()
 })
 test('Check this test case 3', () => {
     render(<App />)
+    console.log("testcase 3")
     const text = screen.getByText(/This is Your Name/i)
     expect(text).toBeInTheDocument()
 })
 test('Check this test case 4', () => {
     render(<App />)
+    console.log("testcase 4")
     console.log("Before all four done")
     const text = screen.getByText(/Hey I Am Akash/i)
     expect(text).toBeInTheDocument()
@@ -39,6 +43,11 @@ test('Check this test case 4', () => {
 
 
 // AfterAll Use ->Excution only one time after all test cases run
-afterAll(()=>{
-    console.log("_________Execution of AfterAll_______")
+// afterAll(()=>{
+//     console.log("_________Execution of AfterAll_______")
+// })
+
+// use the afterEach
+afterEach(()=>{
+    console.log("_____________Execution is Done After Each testCase_____________")
 })
