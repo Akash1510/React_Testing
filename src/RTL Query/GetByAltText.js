86f3e07c3f283eed4f8f0febb5d0aea2f80ae6b0
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2024-09-30 16:34:19 +0530

    Work with the GetByAltText

--- src/RTL Query/GetByAltText.js
+++ src/RTL Query/GetByAltText.js
@@ -6,6 +6,8 @@ const GetByAltText = () => {
         {/* this Quey is Worked on the images */}
         <h1>GetByAltText</h1>
         <img src="https://th.bing.com/th/id/OIP.wbpipwT5-zbihQY2ccFlHQHaE8?rs=1&pid=ImgDetMain" alt="Any" />
+        <img src="https://th.bing.com/th/id/OIP.wbpipwT5-zbihQY2ccFlHQHaE8?rs=1&pid=ImgDetMain" alt="Any" />
+        <img src="https://th.bing.com/th/id/OIP.wbpipwT5-zbihQY2ccFlHQHaE8?rs=1&pid=ImgDetMain" alt="Any" />
     </div>
   )
 }
--- src/__tests__/RTL_Queris/getbyalttext.js
+++ src/__tests__/RTL_Queris/getbyalttext.js
@@ -2,8 +2,18 @@ import { render ,screen} from "@testing-library/react";
 import GetByAltText from "../../RTL Query/GetByAltText";
 
 
-test("Checking of the GetByaltText Functinality",()=>{
+// test("Checking of the GetByaltText Functinality",()=>{
+//     render(<GetByAltText/>)
+//     const altimage  = screen.getByAltText('Any')
+//         expect(altimage).toBeInTheDocument()
+    
+// })
+
+// GetAllByAltText -> Working
+test("Checking the GetAllByAltText",()=>{
     render(<GetByAltText/>)
-    const altimage  = screen.getAllByAltText('Any')
-    expect(altimage).toBeInTheDocument();
+   const altImages = screen.getAllByAltText('Any')
+   for(let i = 0;i<altImages.length;i++){
+    expect(altImages[i]).toBeInTheDocument();
+   }
 })
