2c3d0831930057e5f214b18a8a9e2477235ce696
Akash Jadhav <142928804+Akash1510@users.noreply.github.com>
2024-09-30 14:07:19 +0530

    working with the getAllByDisplayValue()

--- src/RTL Query/GetByDisplay.js
+++ src/RTL Query/GetByDisplay.js
@@ -6,6 +6,8 @@ const GetByDisplay = () => {
         <h1>RTL Query  : GetByDisplay</h1>
         
         <input type="text" defaultValue="Akash" />
+        <input type="text" defaultValue="Akash" />
+        <input type="text" defaultValue="Akash" />
 
 
         </div>
--- src/__tests__/RTL_Queris/getdisplay.js
+++ src/__tests__/RTL_Queris/getdisplay.js
@@ -1,7 +1,19 @@
 import {render,screen} from '@testing-library/react'
 import GetByDisplay from '../../RTL Query/GetByDisplay'
-test ("Working with the Display Values",()=>{
+// test ("Working with the Display Values",()=>{
+//     render(<GetByDisplay/>)
+//     const input = screen.getByDisplayValue('Akash')
+//     expect(input).toBeInTheDocument()
+// })
+
+
+
+// Now Working with the Multiple GetAllByDisplayvalue()
+
+test("Working with the Multiple Display Values",()=>{
     render(<GetByDisplay/>)
-    const input = screen.getByDisplayValue('Akash')
-    expect(input).toBeInTheDocument()
+    const inputMul = screen.getAllByDisplayValue("Akash")
+    for (let i = 0;i<inputMul.length;i++){
+        expect(inputMul[i]).toBeInTheDocument();
+    }
 })
